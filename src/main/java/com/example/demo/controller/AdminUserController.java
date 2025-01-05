0457a82237e4a6c3f8a4c5dfa3a000b1ec05c51c
sasa-0823 <thwsnowman.0823@gmail.com>
2025-01-05 15:42:02 +0900

    ノートに転送

--- src/main/java/com/example/demo/controller/AdminUserController.java
+++ src/main/java/com/example/demo/controller/AdminUserController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 import java.util.List;
 
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.User;
 import com.example.demo.form.UserRegisterForm;
 import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -27,27 +29,29 @@ public class AdminUserController {
 public String adminUser(Model model){
   List<User> users = userService.getAllUsers();
   model.addAttribute("users", users);
+
   if(!model.containsAttribute("userRegisterForm")){
-    model.addAttribute("userRegisterFrom", new UserRegisterForm());
+    model.addAttribute("userRegisterForm", new UserRegisterForm());
   }
   return "adminUserView";
 }
 
 @PostMapping("/register")
 public String registerUser(RedirectAttributes redirectAttributes,
-  @RequestParam("user_name") String userName,
-  @RequestParam("password") String password,
-  @RequestParam("role_id") int roleId) {    
+  @Validated UserRegisterForm form, BindingResult result){  
+    if(result.hasErrors()){
+      redirectAttributes.addFlashAttribute("userRegisterForm", form);
+      redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(form), result);      return "redirect:/adminuser";
+    }  
     try{
-      userService.createUser(userName, password, roleId);
+      userService.createUser(form.getUserName(), form.getPassword(), form.getRoleId());
       redirectAttributes.addFlashAttribute("successMessage", "ユーザー登録が完了しました");
     }
     catch(IllegalArgumentException e){
       //登録失敗時はエラーを出力する
       redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
       //再入力用の入力データをビューに渡す
-      redirectAttributes.addFlashAttribute("userName", userName);
-      redirectAttributes.addFlashAttribute("role_id", roleId);
+      redirectAttributes.addFlashAttribute("userRegisterForm", form);
     }
 
     return "redirect:/adminuser";
--- src/main/java/com/example/demo/form/UserRegisterForm.java
+++ src/main/java/com/example/demo/form/UserRegisterForm.java
@@ -1,11 +1,21 @@
 package com.example.demo.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRegisterForm {
+
+    @NotBlank(message = "ユーザー名を入力してください")  //未入力確認(空文字もケアする)
     private String userName;
+
+    @NotBlank(message = "パスワードを入力してください")
+    @Size(min = 8, message = "パスワードは少なくとも８文字必要です")
     private String password;
+
+    @NotNull(message = "ロールを正しく取得出来きませんでした")  //未入力確認(空文字はケアしない)
     private Integer roleId;
 
 }
